0ae93b6df9f5bf30b6b94431992e3a5b504acec5
byyzgq <1296279026@qq.com>
2022-02-17 17:04:50 +0800

    java-web-gec

---
+++ three-tier/src/main/java/com/cm/service/AjaxDemo.java
@@ -0,0 +1,21 @@
+package com.cm.service;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AjaxDemo extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       String username=req.getParameter("username");
+        System.out.println(username);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
